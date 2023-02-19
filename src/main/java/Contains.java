6b44744c5039750c9a3bfc76ca01a6f293dd0abf
musawir252 <musawir252@gmail.com>
2023-02-19 08:31:51 +0000

    Completed Contains Lab

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -11,6 +11,13 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[] arr, int target){
-        return false;
+        for(int index = 0; index <arr.length; index++){
+            if(arr[index] == target){
+                return true;
+            } 
+        }
+            return false;
+        
+        
     }
 }
